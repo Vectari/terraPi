2cd97bf1713da136ad15fed9358004cd7db5bb8c
Vectari <mateusz.majer996@gmail.com>
2025-01-14 22:51:32 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # terraPi
+
+1. source venv/bin/activate
+2. python app.py
--- components/header.py
+++ components/header.py
@@ -1,2 +1,2 @@
 def render():
-    return "<header><h1>Welcome to My Python App</h1></header>"
+    return "<header><h1>Welcome to My Python App !!</h1></header>"
