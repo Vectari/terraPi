22ceaa172ec87bd072b1604871902be033697bba
Vectari <mateusz.majer996@gmail.com>
2025-08-03 19:12:54 +0200

    feat: update temp

--- components/temperature_sensor.py
+++ components/temperature_sensor.py
@@ -24,5 +24,5 @@ class TemperatureSensor:
         if equals_pos != -1:
             temp_string = lines[1][equals_pos + 2:]
             temp_c = float(temp_string) / 1000.0
-            return {'temperature': round(temp_c, 2)}
+            return {'temperature': round(temp_c, 1)}
         return {'temperature': None}
