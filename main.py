a0df65a8df41cfe07e02ada9172aeedda2b9de7d
Vectari <mateusz.majer996@gmail.com>
2025-08-03 19:12:28 +0200

    feat: add display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from components.led_indicator import LEDIndicator
 from components.temperature_sensor import TemperatureSensor
+from components.display import Display
 import time
 
 def main():
@@ -8,6 +9,7 @@ def main():
     # Inicjalizacja komponentów
     status_led = LEDIndicator(pin=17)
     temp_sensor = TemperatureSensor()
+    display = Display()
 
     status_led.turn_on()
 
@@ -15,12 +17,19 @@ def main():
         while True:
             # Tu w przyszłości będą inne komponenty i logika
             data = temp_sensor.read()
-            print(f"Temperatura: {data['temperature']}°C")
+            temp = data['temperature']
+            print(f"Temperatura: {temp}°C")
+
+            if temp is not None:
+                display.show_text(f"Temp: {temp}°C")
+            else:
+                display.show_text("Brak odczytu")
             time.sleep(5)
 
     except KeyboardInterrupt:
         print("Zamykanie aplikacji...")
         status_led.turn_off()
+        display.clear()
 
 if __name__ == "__main__":
     main()
